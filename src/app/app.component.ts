d0c9032bcac21f7dda9b772e6c72dc15b408ed2d
facundo <facurtprn@gmail.com>
2024-04-29 18:01:37 -0300

    geolocalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   constructor() {}
+
+  ngOnInit(): void {
+    this.printCurrentPosition()
+  }
+
+  public async printCurrentPosition() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coordinates);
+    } catch (error) {
+      console.error('Error getting current position:', error);
+    }
+  }
 }
